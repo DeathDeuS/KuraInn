a06878f20dde287453dd31cc1c7c6aa7e0f1f00a
Jerome <jchansin.tcc@gmail.com>
2018-06-16 15:38:17 -1000

    Sidebar working

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Layout.css';
-/* import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faFacebook from '@fortawesome/fontawesome-free-solid/faFacebook';
-import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
-import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope'; */
  
 class Layout extends Component {
     render() {
@@ -22,30 +18,59 @@ class Layout extends Component {
 
                 {/* Sidebar code */}
                 <div class="icon-bar">
-                    <a href="https://www.facebook.com/KURA-INN-Mangareva-1907947855889011/" target="_blank"><i class="fab fa-facebook-f"/></a> 
-                    <a><i class="fas fa-phone"/></a> 
-                    <a><i class="fas fa-envelope"/></a> 
+                    <a href="https://www.facebook.com/KURA-INN-Mangareva-1907947855889011/" target="_blank" rel="noopener noreferrer" class="btn btn-secondary sidebar-button">
+                        <i class="fab fa-facebook-f"/>
+                    </a> 
+                    <div class="btn-group dropright" id="droprightMenuDiv">
+                        <a class="btn btn-secondary dropdown-toggle sidebar-button" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-phone"/>
+                        </a>
+                        <div class="dropdown-menu">
+                            {/* <a class="dropdown-item" href="#">Action</a> */}
+                            Numéro de téléphone ici
+                        </div>
+                    </div>                    <div class="btn-group dropright" id="droprightMenuDiv">
+                        <a class="btn btn-secondary dropdown-toggle sidebar-button" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-envelope"/>
+                        </a>
+                        <div class="dropdown-menu">
+                            {/* <a class="dropdown-item" href="#">Action</a> */}
+                            Adresse mail là
+                        </div>
+                    </div>
                 </div>
 
                 {/* Footer code */}
-                <div class="row">
-                    <div class="col-1">
-                        Nom : <br/>
-                        E-mail : <br/>
-                        Message : <br/>
-                    </div>
-
-                    <div class="col-11">
-                        <form action="https://formspree.io/your@email.com" 
-                            method="POST">
-                            <input type="text" name="name" required="required"/>
-                            <br/>
-                            <input type="email" name="_replyto" required="required"/>
-                            <br/>
-                            <input type="text" name="message" required="required"/>
-                            <input type="submit" value="Envoyer"/>
-                        </form> 
-                    </div>
+                <div class="footer">
+                Pour nous contacter, veuillez remplir ce formulaire :
+                    <form action="https://formspree.io/your@email.com" 
+                        method="POST">
+                        <div class="row">
+                            <div class="col-2">
+                            Nom : 
+                            </div>
+                            <div class="col-10">
+                                <input type="text" name="name" required="required"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                            Prénom : 
+                            </div>
+                            <div class="col-10">
+                                <input type="email" name="_replyto" required="required"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2">
+                            Message : 
+                            </div>
+                            <div class="col-10">
+                                <input type="text" name="message" required="required"/>
+                                <input type="submit" value="Envoyer"/>
+                            </div>
+                        </div>
+                    </form> 
                 </div>
             </div>
             

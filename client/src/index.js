ba697dff467897bbc69553d47acbca8f3f3c845a
Jerome <jchansin.tcc@gmail.com>
2018-06-23 08:56:02 -1000

    install react-gh pages

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 registerServiceWorker();
